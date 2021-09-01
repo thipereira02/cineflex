522376faf756aa9cda79b05c58dee95798ed2954
thipereira02 <thiago.spereira02@gmail.com>
2021-09-01 16:34:29 -0300

    Initial files

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+export default function App() {
+	return (
+		<BrowserRouter>
+			<Switch>
+				<Route>
+                    
+				</Route>
+			</Switch>
+		</BrowserRouter>
+	);
+}
