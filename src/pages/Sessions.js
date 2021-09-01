865eb291095aa3578a36eeedccdac6ba50eb3024
thipereira02 <thiago.spereira02@gmail.com>
2021-09-01 18:49:14 -0300

    Sending request to catch movie data

--- src/pages/Sessions.js
+++ src/pages/Sessions.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
 
 import PageTitle from "../components/common/PageTitle";
 import Body from "../components/common/Body";
+import { useParams } from "react-router";
 
 export default function Sessions() {
+	const { movieId } = useParams();
+	console.log(movieId);
+
+	useEffect(() => {
+		const request = axios.get(`https://mock-api.bootcamp.respondeai.com.br/api/v3/cineflex/movies/${movieId}/showtimes`);
+		request.then(res => {
+			console.log(res.data);
+		});
+	},[]);
+
 	return (
 		<>
 			<PageTitle text={"Selecione o horário"} />
